56ef92bbb95bd8c248bf75fa902a66f26b1b945a
GuiMitter <202508435977@alunos.estacio.br>
2025-08-15 20:55:52 +0000

    operadores de atribuicao

---
+++ teste_operadores_atribuicao.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero1 = 5, numero2 = 12, resultado;
+
+
+
+    resultado = 10;
+    printf("Resultado: %i\n", resultado);
+
+    resultado +=20;
+    printf("Resultado: %i\n", resultado);
+
+    resultado -=numero1;
+    printf("Resultado: %i\n", resultado);
+
+    resultado *=numero2;
+    printf("Resultado: %i\n", resultado);
+
+    resultado /=numero1;
+    printf("Resultado: %i\n", resultado);
+
+    
+
+return 0;
+
+}
