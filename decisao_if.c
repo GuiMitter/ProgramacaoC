6542e2a38ddfe0f6d5d5cc501aca6139b099fd55
GuiMitter <202508435977@alunos.estacio.br>
2025-08-17 16:28:35 +0000

    teste if e else

---
+++ decisao_if.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    
+    int numero1 = 4, numero2 = 5;
+
+    if (numero1 > numero2) {
+        printf("O número %d é maior que o número %d\n", numero1, numero2);
+    }  else{
+          printf("O número %d é menor que o número %d\n", numero1, numero2);
+    }
+    
+
+    return 0;
+}
