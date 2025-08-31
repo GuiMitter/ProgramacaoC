f4d1ff3275651f4fa053f9031fc3e175ca4771c2
GuiMitter <202508435977@alunos.estacio.br>
2025-08-31 22:01:12 +0000

    criação de pasta super trufo, e também documento com basico do while

---
+++ while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+    int i = 1;
+
+
+    while (i<=5)//condição é booleana, enquanto a condição é verdadeira o loop será executado ]
+    {   
+        printf("%i\n",i);
+        i++;
+        
+    }
+    
+
+    return ;
+}
